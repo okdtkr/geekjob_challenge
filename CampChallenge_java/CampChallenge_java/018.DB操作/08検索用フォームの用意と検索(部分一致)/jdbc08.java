d79903274ec383fda4c507333e42a3909e85410d
TKRKD\Takuro <okdtkr>
2018-07-19 09:45:07 +0900

    Java 課題　検索用フォームの用意と検索(部分一致)　を提出いたします

---
+++ CampChallenge_java/CampChallenge_java/018.DB操作/08検索用フォームの用意と検索(部分一致)/jdbc08.java
@@ -0,0 +1,148 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *検索用フォームの用意と検索（部分一致）
+ * @author Takuro
+ */
+public class jdbc08 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet jdbc08</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet jdbc08 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            
+        request.setCharacterEncoding("UTF-8");
+        
+        String name =request.getParameter("human name");    
+        Connection con = null;
+        PreparedStatement st =null;
+        ResultSet data =null;
+        
+        out.print(name);
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            // MySQLに接続
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db", "OKADATAKURO", "OKDTKR");
+            out.println("MySQLに接続できました。");
+            
+            
+            st=con.prepareStatement("select* from profiles where name like ?");
+            st.setString(1,"%"+name+"%");
+
+            data=st.executeQuery();
+            while(data.next()){
+                out.print(data.getString("profilesID")+" ");
+                out.print(data.getString("name")+" ");
+                out.print(data.getString("tell")+" ");
+                out.print(data.getString("age")+" ");
+                out.println(data.getString("birthday")+" ");
+            }
+            
+//            System.out.print("aaaa"+ st.executeUpdate());
+//            stm=con.createStatement();
+//            int result=stm.executeUpdate
+//            ("insert into profiles values(3,'鈴木 実','080-5566-7788',24,'2000-12-24')");
+//            System.out.println("更新回数："+ result);
+            
+//            int result2=stm.executeUpdate("delete from profiles where profilesID=3");
+//            System.out.println("更新回数："+ result2);
+
+            
+            data.close();
+            st.close();
+            con.close();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            System.out.println("JDBCドライバのロードに失敗しました。");
+        } catch (SQLException e) {
+            System.out.println("MySQLに接続できませんでした。");
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                    System.out.println("MySQLのクローズに失敗しました。");
+                }
+            }
+        }
+    
+        }
+    }
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
